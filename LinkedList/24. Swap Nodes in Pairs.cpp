c7038560aa26d3153e067d76793b24d20bc174b9
Tomcat <pine21708@gmail.com>
2024-06-14 22:58:04 +0800

    [LinkedList] 24. Swap Nodes in Pairs

--- LinkedList/24. Swap Nodes in Pairs.cpp
+++ LinkedList/24. Swap Nodes in Pairs.cpp
@@ -8,6 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
+ * Case1: 
+ */
+
+class Solution {
+public:
+    struct ListNode* swapPairs(struct ListNode* head)
+    {
+        struct ListNode* virtualHead = new ListNode(0, head);
+
+        struct ListNode* ptr = virtualHead;
+
+        struct ListNode* node1 = nullptr;
+        struct ListNode* node2 = nullptr;
+
+        while(nullptr != head)
+        {
+            node1 = head;
+            head = head->next;
+
+            if(nullptr == head)
+            {
+                break;
+            }
+
+            node2 = head;
+            head = head->next;
+
+            ptr->next = node2;
+            node2->next = node1;
+            node1->next = head;
+
+            ptr = node1;
+        }
+
+        head = virtualHead->next;
+        delete virtualHead;
+
+        return head;
+    }
+};
+
+/*
+ * Case2: Use [92] reverseBetween function 
+ */
+
 class Solution {
 public:
     struct ListNode* swapPairs(struct ListNode* head)
