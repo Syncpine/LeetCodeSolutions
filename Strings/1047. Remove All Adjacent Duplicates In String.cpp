5e4f38cd93f563fe5decc3a1df3ebd38691629a8
Tomcat <pine21708@gmail.com>
2024-08-01 23:15:57 +0800

    [Strings] 1047. Remove All Adjacent Duplicates In String

--- Strings/1047. Remove All Adjacent Duplicates In String.cpp
+++ Strings/1047. Remove All Adjacent Duplicates In String.cpp
@@ -1,3 +1,49 @@
+/*
+ * Case1: 
+ */
+
+class Solution {
+public:
+    std::string removeDuplicates(const std::string& s)
+    {
+        std::string ret = "";
+        int pos = -1;
+
+        const int strLength = s.length();
+        int index = 0;
+
+        while(index < strLength)
+        {
+            if(ret == "")
+            {
+                ret += s[index];
+                ++pos;
+            }
+            else
+            {
+                if(ret[pos] == s[index])
+                {
+                    ret.erase(pos);
+                    --pos;
+                }
+                else
+                {
+                    ret += s[index];
+                    ++pos;
+                }
+            }
+
+            ++index;
+        }
+
+        return ret;
+    }
+};
+
+/*
+ * Case2: Use std::stack
+ */
+
 class Solution {
 public:
     std::string removeDuplicates(const std::string& s)
