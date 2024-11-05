228cc7cd862b4c64df495c4b3d7a44e3347d9d6f
Tomcat <pine21708@gmail.com>
2024-11-05 23:00:23 +0800

    [Strings] 2697. Lexicographically Smallest Palindrome

---
+++ Strings/2697. Lexicographically Smallest Palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    std::string makeSmallestPalindrome(std::string& s)
+    {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start < end)
+        {
+            if(s[start] < s[end])
+            {
+                s[end] = s[start];
+            }
+            else if(s[start] > s[end])
+            {
+                s[start] = s[end];
+            }
+
+            ++start;
+            --end;
+        }
+
+        return s;
+    }
+};
