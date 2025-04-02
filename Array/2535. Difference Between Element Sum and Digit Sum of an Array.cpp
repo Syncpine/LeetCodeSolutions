18a0cc595b30120da657fa60b6e5421001ec6f9e
Tomcat <pine21708@gmail.com>
2025-04-02 20:51:45 +0800

    [Array] 2535. Difference Between Element Sum and Digit Sum of an Array

---
+++ Array/2535. Difference Between Element Sum and Digit Sum of an Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int differenceOfSum(const std::vector<int>& nums)
+    {
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for(auto num : nums)
+        {
+            elementSum += num;
+
+            while(num > 0)
+            {
+                digitSum += num % 10;
+                num /= 10;
+            }
+        }
+
+        return std::abs(elementSum - digitSum);
+    }
+};
