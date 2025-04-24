ac629c4270ce2d652a011bf6e153f9b32182b11b
Tomcat <pine21708@gmail.com>
2025-04-24 22:35:24 +0800

    [Array] 3024. Type of Triangle

---
+++ Array/3024. Type of Triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    std::string triangleType(std::vector<int>& nums)
+    {
+        std::sort(nums.begin(), nums.end());
+
+        if(nums[0] + nums[1] <= nums[2])
+        {
+            return "none";
+        }
+
+        if(nums[0] == nums[2])
+        {
+            return "equilateral";
+        }
+
+        if(nums[0] == nums[1] || nums[1] == nums[2])
+        {
+            return "isosceles";
+        }
+
+        return "scalene";
+    }
+};
